757173e62e8d4ce4b1be41bf324c63c7b61bdd1a
Sebastian Wallin <sebastian.wallin@gmail.com>
2012-10-08 21:13:57 +0200

    Whitespace celanup in config + heroku buildpack instructions in README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Middleman template
+
+## Heroku deployment
+Don't forget to add the middleman buildpack to your app
+
+    heroku config:add BUILDPACK_URL=http://github.com/indirect/heroku-buildpack-middleman.git
--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 activate :directory_indexes
 
-### 
+###
 # Compass
 ###
 
@@ -18,13 +18,13 @@ activate :directory_indexes
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -59,21 +59,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
